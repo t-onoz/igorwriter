a6ea851481762351715cdc78120b3e04caabc9f3
t-onoz <18749921+t-onoz@users.noreply.github.com>
2019-11-16 12:52:39 +0900

    added dict_to_{ibw, itx}

--- igorwriter/utils.py
+++ igorwriter/utils.py
@@ -4,38 +4,38 @@ import igorwriter
 from igorwriter import IgorWave
 
 
-def dataframe_to_itx(dataframe, path_or_buf, on_errors='fix'):
+def dict_to_itx(dict_like, path_or_buf, on_errors='fix'):
     """
 
-    :type dataframe: pandas.DataFrame
+    :param dict_like: dictionary-like object ({name: array-like, ...})
     :param path_or_buf: filename or file-like object
     :param on_errors: behavior when invalid name is given. 'fix': fix errors. 'raise': raise an exception.
-    :return: dictionary of generated waves ({column: wave, ...}
+    :return: dictionary of generated waves ({name: wave, ...})
     """
-    waves = dict()
     fp = path_or_buf if hasattr(path_or_buf, 'write') else open(path_or_buf, 'w', encoding=igorwriter.ENCODING)
+    wavelist = [IgorWave(array, name, on_errors) for (name, array) in dict_like.items()]
     try:
-        for column, series in dataframe.items():
-            wave = IgorWave(series, column, on_errors=on_errors)
-            waves[column] = wave
+        for wave in wavelist:
             wave.save_itx(fp)
     finally:
         if fp is not path_or_buf:
             fp.close()
-    return waves
+    return {wave.name: wave for wave in wavelist}
 
 
-def dataframe_to_ibw(dataframe, prefix, on_errors='fix'):
+def dict_to_ibw(dict_like, prefix, on_errors='fix'):
     """
 
-    :type dataframe: pandas.DataFrame
+    :param dict_like: dictionary-like object ({name: array-like, ...})
     :param prefix: file name prefix (each wave is saved as <prefix>_<column>.ibw)
     :param on_errors: behavior when invalid name is given. 'fix': fix errors. 'raise': raise an exception.
-    :return: dictionary of generated waves ({column: wave, ...}
+    :return: dictionary of generated waves ({name: wave, ...})
     """
-    waves = dict()
-    for column, series in dataframe.items():
-        wave = IgorWave(series, column, on_errors=on_errors)
-        wave.save(str(prefix) + '_%s.ibw' % column)
-        waves[column] = wave
-    return waves
+    wavelist = [IgorWave(array, name, on_errors) for (name, array) in dict_like.items()]
+    for wave in wavelist:
+        wave.save(str(prefix) + '_%s.ibw' % wave.name)
+    return {wave.name: wave for wave in wavelist}
+
+
+dataframe_to_itx = dict_to_itx
+dataframe_to_ibw = dict_to_ibw
